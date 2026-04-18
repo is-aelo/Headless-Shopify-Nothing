08a7f4e798f848da31cfd307923c31a4d679fe93
Eloisa Jane Talingting <talingting.eloise@gmail.com>
2026-04-18 16:02:35 +0800

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,7 +2,6 @@ import { CartProvider } from "components/cart/cart-context";
 import Footer from "components/layout/footer";
 import { MainWrapper } from "components/layout/main-wrapper";
 import { Navbar } from "components/layout/navbar";
-import SmoothScroll from "components/SmoothScroll";
 import { getCart } from "lib/shopify";
 import { baseUrl } from "lib/utils";
 import { ReactNode, Suspense } from "react";
@@ -48,14 +47,13 @@ export default async function RootLayout({
         ${ndot57Caps.variable} 
         ${nType82.variable} 
         ${shareTechMono.variable}
-        scroll-smooth
       `}
     >
       <body className="antialiased selection:bg-black selection:text-white">
         <CartProvider cartPromise={cart}>
           <Navbar />
           <MainWrapper>
-            <SmoothScroll>{children}</SmoothScroll>
+            {children}
             <Suspense fallback={<div className="h-40 w-full" />}>
               <Footer />
             </Suspense>
